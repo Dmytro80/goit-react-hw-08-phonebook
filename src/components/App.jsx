5ddd2c525ea0637bd9f214ee3865e7d34b04f4f6
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-10-28 20:29:40 +0200

    add use Effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ContactForm from './contactForm';
 import ContactList from './contactList';
 import Filter from './filter';
@@ -16,18 +16,16 @@ export const App = () => {
 
   const [filterQuery, setFilterQuery] = useState('');
 
+  const contactsOfStorage = useRef(
+    JSON.parse(localStorage.getItem(CONTACTS_STORAGE_KEY))
+  );
+
   useEffect(() => {
-    const contactsOfStorage = JSON.parse(
-      localStorage.getItem(CONTACTS_STORAGE_KEY)
-    );
-
-    console.log(contactsOfStorage);
-
-    if (!contactsOfStorage) {
+    if (!contactsOfStorage.current) {
       return;
     }
 
-    setContact(contactsOfStorage);
+    setContact(contactsOfStorage.current);
   }, []);
 
   useEffect(() => {
@@ -76,87 +74,3 @@ export const App = () => {
     </Wrapper>
   );
 };
-
-export class OldApp extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  componentDidMount() {
-    const contactsOfStorage = JSON.parse(
-      localStorage.getItem(CONTACTS_STORAGE_KEY)
-    );
-
-    if (contactsOfStorage) {
-      this.setState({ contacts: contactsOfStorage });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem(
-        CONTACTS_STORAGE_KEY,
-        JSON.stringify(this.state.contacts)
-      );
-    }
-  }
-
-  handleFilterInputChange = e => {
-    const { value } = e.currentTarget;
-    this.setState({ filter: value });
-  };
-
-  formSubmitHandler = contact => {
-    const { contacts } = this.state;
-    const normalizedContactName = contact.name.toLocaleLowerCase();
-    const bookContainsName = contacts.filter(contact => {
-      return contact.name.toLocaleLowerCase() === normalizedContactName;
-    });
-
-    if (bookContainsName.length > 0) {
-      return alert(`${contact.name} is already in contacts.`);
-    }
-
-    this.setState(prevState => ({
-      contacts: [contact, ...prevState.contacts],
-    }));
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizedFilter = filter.toLocaleLowerCase();
-    return contacts.filter(contact => {
-      return contact.name.toLocaleLowerCase().includes(normalizedFilter);
-    });
-  };
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-
-    return (
-      <Wrapper>
-        <Title>Phonebook</Title>
-        <ContactForm onSubmitContactForm={this.formSubmitHandler} />
-        <SecondTitle>Contacts</SecondTitle>
-        <Filter filter={filter} onInputChange={this.handleFilterInputChange} />
-        <ContactList
-          contacts={visibleContacts}
-          onDeleteContact={this.deleteContact}
-        />
-      </Wrapper>
-    );
-  }
-}
